befde58c299b7e08045201c26509b43c7a989435
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-01-14 22:15:52 -0600

    19 - Cómo crear rutas con route resource

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,7 @@ use App\Http\Controllers\CursoController;
 
 Route::get('/',HomeController::class);
 
-Route::get('cursos',[CursoController::class,'index'])->name('cursos.index');
+//Route::resource('cursos', CursoController::class);
 
-Route::get('cursos/create',[CursoController::class,'create'])->name('cursos.create');
 
-Route::post('cursos',[CursoController::class,'store'])->name('cursos.store');
-
-Route::get('cursos/{curso}',[CursoController::class,'show'])->name('cursos.show');
-
-Route::get('cursos/{curso}/edit',[CursoController::class,'edit'])->name('cursos.edit');
-
-Route::put('cursos/{curso}',[CursoController::class,'update'])->name('cursos.update');
-
-Route::delete('cursos/{curso}',[CursoController::class,'destroy'])->name('cursos.destroy');
+Route::resource('asignaturas',CursoController::class)->names('cursos');
